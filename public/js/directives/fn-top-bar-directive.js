25fa8c75cebfc2a70213e3d978e63e9702f264d2
Lucas Piscello <lpiscello@gmail.com>
2015-02-15 11:11:44 -0200

    Menu da felicidade eterna!

--- public/js/directives/fn-top-bar-directive.js
+++ public/js/directives/fn-top-bar-directive.js
@@ -9,6 +9,20 @@
             templateUrl: '../../templates/partials/topBar.html',
             controller: function ($scope) {
 
+                var menuState = false;
+
+                $scope.switchMenu = function() {
+
+                    if(menuState) {
+
+                        angular.element('.top-bar-menu-container').hide();
+                        menuState = false;
+                    } else {
+
+                        angular.element('.top-bar-menu-container').show();
+                        menuState = true;
+                    }
+                };
             }
         };
     }
