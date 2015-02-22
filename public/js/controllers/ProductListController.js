0e258db5c54f5abb54558a8cc304312b7f4a9c0b
Lucas Piscello <lpiscello@gmail.com>
2015-02-22 05:10:05 -0300

    Itsy bitsy details

--- public/js/controllers/ProductListController.js
+++ public/js/controllers/ProductListController.js
@@ -27,6 +27,20 @@
 
             console.log('Hold up! This doesn\'t exist yet :)');
         };
+
+        $scope.moveToTop = function() {
+
+            angular.element("html, body").animate({ scrollTop: 0 }, "slow");
+        };
+
+        angular.element(window).scroll(function(){
+            if (angular.element(window).scrollTop() > 100){
+                angular.element('#scrollTop').fadeIn(250);
+            } else {
+                angular.element('#scrollTop').fadeOut(250);
+            }
+        });
+        angular.element('#scrollTop').fadeOut(250);
     }
 
     angular.module('app.controllers').controller('ProductListController', productListController);
