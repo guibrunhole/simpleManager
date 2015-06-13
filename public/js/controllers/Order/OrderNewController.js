04b020120ceb377238c59561dcaa403ff49dac77
Lucas <lpiscello@gmail.com>
2015-06-12 21:28:44 -0300

    modal add pedido ftw!

---
+++ public/js/controllers/Order/OrderNewController.js
@@ -0,0 +1,11 @@
+(function() {
+
+    'use strict';
+
+    function orderNewCtrl($scope) {
+
+        $scope.wut = 'suuuuup!';
+    }
+
+    angular.module('app.controllers').controller('OrderNewController', orderNewCtrl)
+})();
