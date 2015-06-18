649631ded51fb4b8fda4d20c57551814c13d19d5
Guilherme <gui.brunhole@gmail.com>
2015-06-18 11:09:08 -0300

    Fixing freaking form :poop:

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -6,41 +6,30 @@
 
         $scope.setLocationTitle('Home');
 
-        var chart1 = c3.generate({
+        var chart = c3.generate({
             data: {
+                x: 'x',
+//        xFormat: '%Y%m%d', // 'xFormat' can be used as custom format of 'x'
                 columns: [
-                    ['data3', 30],
-                    ['data4', 120],
-                ],
-                type : 'donut',
-                onclick: function (d, i) { console.log("onclick", d, i); },
-                onmouseover: function (d, i) { console.log("onmouseover", d, i); },
-                onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+                    ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
+//            ['x', '20130101', '20130102', '20130103', '20130104', '20130105', '20130106'],
+                    ['data1', 30, 200, 100, 400, 150, 250],
+                    ['data2', 130, 340, 200, 500, 250, 350]
+                ]
             },
-            donut: {
-                title: "Iris Petal Width"
+            axis: {
+                x: {
+                    type: 'timeseries',
+                    tick: {
+                        format: '%Y-%m-%d'
+                    }
+                }
+            },
+            types:{
+                data1: 'spline',
+                data2: 'area'
             }
         });
-
-        setTimeout(function () {
-            chart1.load({
-                columns: [
-                    ["setosa", 0.2, 0.2, 0.2, 0.2, 0.2, 0.4, 0.3, 0.2, 0.2, 0.1, 0.2, 0.2, 0.1, 0.1, 0.2, 0.4, 0.4, 0.3, 0.3, 0.3, 0.2, 0.4, 0.2, 0.5, 0.2, 0.2, 0.4, 0.2, 0.2, 0.2, 0.2, 0.4, 0.1, 0.2, 0.2, 0.2, 0.2, 0.1, 0.2, 0.2, 0.3, 0.3, 0.2, 0.6, 0.4, 0.3, 0.2, 0.2, 0.2, 0.2],
-                    ["versicolor", 1.4, 1.5, 1.5, 1.3, 1.5, 1.3, 1.6, 1.0, 1.3, 1.4, 1.0, 1.5, 1.0, 1.4, 1.3, 1.4, 1.5, 1.0, 1.5, 1.1, 1.8, 1.3, 1.5, 1.2, 1.3, 1.4, 1.4, 1.7, 1.5, 1.0, 1.1, 1.0, 1.2, 1.6, 1.5, 1.6, 1.5, 1.3, 1.3, 1.3, 1.2, 1.4, 1.2, 1.0, 1.3, 1.2, 1.3, 1.3, 1.1, 1.3],
-                    ["virginica", 2.5, 1.9, 2.1, 1.8, 2.2, 2.1, 1.7, 1.8, 1.8, 2.5, 2.0, 1.9, 2.1, 2.0, 2.4, 2.3, 1.8, 2.2, 2.3, 1.5, 2.3, 2.0, 2.0, 1.8, 2.1, 1.8, 1.8, 1.8, 2.1, 1.6, 1.9, 2.0, 2.2, 1.5, 1.4, 2.3, 2.4, 1.8, 1.8, 2.1, 2.4, 2.3, 1.9, 2.3, 2.5, 2.3, 1.9, 2.0, 2.3, 1.8],
-                ]
-            });
-        }, 1500);
-
-        setTimeout(function () {
-            chart1.unload({
-                ids: 'data3'
-            });
-            chart1.unload({
-                ids: 'data4'
-            });
-        }, 2500);
-
 
         //GraphService.loadMain()
         //    .success(function(json) {
