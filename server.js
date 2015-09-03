2b680050027396520b43d8c6f1abcc7127971425
Lucas Piscello <lpiscello@gmail.com>
2015-09-02 21:23:30 -0300

    CORSgit add -A!

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
     var express = require('express');
     var path = require('path');
     var bodyParser = require('body-parser');
+    var cors = require('cors');
 
     var mysql = require('mysql');
     var pool  = mysql.createPool({
@@ -18,7 +19,7 @@
     });
 
     var app = express();
-
+    app.use(cors());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
 
