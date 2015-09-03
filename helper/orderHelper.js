e88fd78f68640da612f8d40e56a6f3cdd3528f39
Lucas Piscello <lpiscello@gmail.com>
2015-09-03 02:35:58 -0300

    Change in email build

--- helper/orderHelper.js
+++ helper/orderHelper.js
@@ -109,7 +109,7 @@
 
             return '<html>' +
                         '<head>' +
-                            '<link rel="stylesheet" href="http://localhost:' + (process.env.SV_PORT || 3010) + '/libs/bootstrap/css/bootstrap.css" media="print">' +
+                            '<link rel="stylesheet" href="' + process.env.BASE_API_ADDRESS + '/libs/bootstrap/css/bootstrap.css" media="print">' +
                         '</head>' +
                         '<body>' +
                             '<div class="row">' +
