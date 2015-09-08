b12231178a5cbf084c731497862a3a219eb91123
Lucas Piscello <lpiscello@gmail.com>
2015-09-08 20:36:19 -0300

    take that report in the face!

--- helper/orderHelper.js
+++ helper/orderHelper.js
@@ -10,43 +10,43 @@
 
         function buildDeliveryHeader(order) {
 
-            return  '<div class="col-xs-12">' +
-                        '<div class="col-xs-12">' +
-                            '<label>Casa de oração:</label> ' + order.churchName +
-                        '</div>' +
-                    '</div>' +
-                    '<div class="col-xs-12">' +
-                        '<div class="col-xs-12">' +
-                            '<label>Endereço:</label> ' + order.churchAddress +
-                        '</div>' +
-                    '</div>' +
-                    '<div class="col-xs-12">' +
-                        '<div class="col-xs-3">' +
-                            '<label>Cidade:</label> ' + order.churchCity +
-                        '</div>' +
-                        '<div class="col-xs-2">' +
-                            '<label>UF:</label> ' + order.churchState +
-                        '</div>' +
-                        '<div class="col-xs-3">' +
-                            '<label>CEP:</label> ' + order.churchZipCode +
-                        '</div>' +
-                        '<div class="col-xs-4">' +
-                            '<label>CNPJ:</label> ' + order.churchCnpj +
-                        '</div>' +
-                    '</div>' +
-                    '<div class="col-xs-12">' +
-                        '<div class="col-xs-8">' +
-                            '<label>Comprador:</label> ' + order.buyerName +
-                        '</div>' +
-                        '<div class="col-xs-4">' +
-                            '<label>Telefone:</label> ' + order.churchPhoneNumber +
-                        '</div>' +
-                    '</div>' +
-                    '<div class="col-xs-12">' +
-                        '<div class="col-xs-12">' +
-                            '<label>Observação:</label> ' + order.orderObservation +
-                        '</div>' +
-                    '</div>';
+            return  "<div class='col-xs-12'>" +
+                        "<div class='col-xs-12'>" +
+                            "<label>Casa de oração:</label> " + order.churchName +
+                        "</div>" +
+                    "</div>" +
+                    "<div class='col-xs-12'>" +
+                        "<div class='col-xs-12'>" +
+                            "<label>Endereço:</label> " + order.churchAddress +
+                        "</div>" +
+                    "</div>" +
+                    "<div class='col-xs-12'>" +
+                        "<div class='col-xs-3'>" +
+                            "<label>Cidade:</label> " + order.churchCity +
+                        "</div>" +
+                        "<div class='col-xs-2'>" +
+                            "<label>UF:</label> " + order.churchState +
+                        "</div>" +
+                        "<div class='col-xs-3'>" +
+                            "<label>CEP:</label> " + order.churchZipCode +
+                        "</div>" +
+                        "<div class='col-xs-4'>" +
+                            "<label>CNPJ:</label> " + order.churchCnpj +
+                        "</div>" +
+                    "</div>" +
+                    "<div class='col-xs-12'>" +
+                        "<div class='col-xs-8'>" +
+                            "<label>Comprador:</label> " + order.buyerName +
+                        "</div>" +
+                        "<div class='col-xs-4'>" +
+                            "<label>Telefone:</label> " + order.churchPhoneNumber +
+                        "</div>" +
+                    "</div>" +
+                    "<div class='col-xs-12'>" +
+                        "<div class='col-xs-12'>" +
+                            "<label>Observação:</label> " + order.orderObservation +
+                        "</div>" +
+                    "</div>";
         }
 
         var unities = {
@@ -58,11 +58,11 @@
         };
 
         function getProductRow(detail){
-            return  '<tr>' +
-                        '<td>' + detail.productId + '</td>' +
-                        '<td>' + detail.productName + '</td>' +
-                        '<td>' + detail.productQuantity + ' ' + unities[detail.productUnity] +   '</td>' +
-                    '</tr>';
+            return  "<tr>" +
+                        "<td>" + detail.productId + "</td>" +
+                        "<td>" + detail.productName + "</td>" +
+                        "<td>" + detail.productQuantity + ' ' + unities[detail.productUnity] +   "</td>" +
+                    "</tr>";
         }
 
         function getProductTableContents(orderDetail) {
@@ -79,18 +79,18 @@
 
         function getProductsTable(orderDetail) {
 
-            return '<table class="table table-striped">' +
-                        '<thead>' +
-                            '<tr>' +
-                                '<th>Código</th>' +
-                                '<th>Descrição</th>' +
-                                '<th>Quantidade</th>' +
-                            '</tr>' +
-                        '</thead>' +
-                        '<tbody>' +
+            return "<table class='table table-striped' style='font-size: 1.2em;'>" +
+                        "<thead>" +
+                            "<tr>" +
+                                "<th>Código</th>" +
+                                "<th>Descrição</th>" +
+                                "<th>Quantidade</th>" +
+                            "</tr>" +
+                        "</thead>" +
+                        "<tbody>" +
                              getProductTableContents(orderDetail) +
-                        '</tbody>' +
-                    '</table>';
+                        "</tbody>" +
+                    "</table>";
         }
 
         function currentDate(){
@@ -107,59 +107,56 @@
 
         function parseToHtml(order) {
 
-            var parsedHtml = '<html>' +
-                '<head>' +
-                '<link rel="stylesheet" href="' + process.env.BASE_API_ADDRESS + '/libs/bootstrap/css/bootstrap.css" media="print">' +
-                '</head>' +
-                '<body>' +
-                '<div class="row">' +
-                '<h1 class="text-center">' +
-                'CONGREGAÇÃO CRISTÃ NO BRASIL' +
-                '</h1>' +
-                '</div>' +
-                '<div class="row">' +
-                '<h2 class="text-center">' +
-                'Pedido de compra - '+ currentDate() +
-                '</h2>' +
-                '</div>' +
-                '<hr />' +
-                '<div class="row">' +
-                '<div class="col-xs-12">' +
-                '<h4>' +
-                'Local de entrega' +
-                '</h4>' +
-                '</div>' +
-                buildDeliveryHeader(order) +
-                '</div>' +
-                '<hr />' +
-                '<div class="row">' +
-                '<div class="col-xs-12">' +
-                '<h4>' +
-                'Resumo da compra' +
-                '</h4>' +
-                '</div>' +
-                '<div class="col-xs-12">' +
-                '<div class="col-xs-12">' +
-                getProductsTable(order.orderDetail) +
-                '</div>' +
-                '</div>' +
-                '</div>' +
-                '<hr />' +
-                '<div class="row">' +
-                '<div class="col-xs-5 col-xs-offset-1 text-center">' +
-                '<p>_________________________________________</p>' +
-                '<label>Assinatura Responsável</label>' +
-                '</div>' +
-                '<div class="col-xs-5 text-center">' +
-                '<p>_________________________________________</p>' +
-                '<label>Assinatura Comissão de Compras</label>' +
-                '</div>' +
-                '</div>' +
-                '</body>' +
-                '</html>';
-
-
-            console.log("Finished parsing html");
+            var parsedHtml = "<html>" +
+                "<head>" +
+                    "<link rel='stylesheet' href='" + process.env.BASE_API_ADDRESS + "/libs/bootstrap/css/bootstrap.css' media='print'>" +
+                "</head>" +
+                "<body style='font-size: 1.1em;'>" +
+                    "<div class='row'>" +
+                        "<h1 class='text-center'>" +
+                        "CONGREGAÇÃO CRISTÃ NO BRASIL" +
+                        "</h1>" +
+                    "</div>" +
+                    "<div class='row'>" +
+                        "<h2 class='text-center'>" +
+                        "Pedido de compra - " + currentDate() +
+                        "</h2>" +
+                    "</div>" +
+                    "<hr />" +
+                    "<div class='row'>" +
+                        "<div class='col-xs-12'>" +
+                            "<h4>" +
+                            "Local de entrega" +
+                            "</h4>" +
+                        "</div>" +
+                        buildDeliveryHeader(order) +
+                    "</div>" +
+                    "<hr />" +
+                    "<div class='row'>" +
+                        "<div class='col-xs-12'>" +
+                            "<h4>" +
+                                "Resumo da compra" +
+                            "</h4>" +
+                        "</div>" +
+                        "<div class='col-xs-12'>" +
+                            "<div class='col-xs-12'>" +
+                                getProductsTable(order.orderDetail) +
+                            "</div>" +
+                        "</div>" +
+                    "</div>" +
+                    "<hr />" +
+                    "<div class='row'>" +
+                        "<div class='col-xs-5 col-xs-offset-1 text-center'>" +
+                            "<p>_________________________________________</p>" +
+                            "<label>Assinatura Responsável</label>" +
+                        "</div>" +
+                        "<div class='col-xs-5 text-center'>" +
+                            "<p>_________________________________________</p>" +
+                            "<label>Assinatura Comissão de Compras</label>" +
+                        "</div>" +
+                    "</div>" +
+                "</body>" +
+            "</html>";
 
             return parsedHtml;
         }
@@ -169,10 +166,7 @@
             createPdf: function(order) {
 
                 var deferred = q.defer();
-
-
                 var createdPdfPath = 'temp/' + uuid.v4() + '.pdf';
-                console.log("Started building report!");
                 var pdf = new NodePDF(null, createdPdfPath, {
                     'content': parseToHtml(order),
                     'viewportSize': {
@@ -186,7 +180,7 @@
                         },
                         'footer': {
                             'height': '1cm',
-                            'contents': '<div style="text-align: right;">{currentPage} / {pages}</div>'
+                            'contents': "<div style='text-align: right;'>{currentPage} / {pages}</div>"
                         }
                     },
                     'zoomFactor': 1
